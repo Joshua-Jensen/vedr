227fc2188e7450029220d096a91df67c88d95772
josh <joshjj3@gmail.com>
2023-03-08 16:59:08 -0700

    no clue

--- app/AppState.js
+++ app/AppState.js
@@ -1,3 +1,4 @@
+import { Snacks } from "./Models/Snacks.js"
 import { Value } from "./Models/Value.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
@@ -6,6 +7,14 @@ import { loadState } from "./Utils/Store.js"
 class AppState extends EventEmitter {
   /** @type {import('./Models/Value').Value[]} */
   values = loadState('values', [Value])
+  /** @type {import('Models/Snacks').Snacks[]} */
+  snacks = [
+    new Snacks({ name: 'doritos', price: 4, remaining: 12, img: 'assets/img/logo.png', }),
+    new Snacks({ name: 'lays', price: 3, remaining: 12, img: 'assets/img/lays.png', }),
+
+  ]
+
+  money = 0
 }
 
 export const appState = new Proxy(new AppState(), {
---
+++ app/Services/SnacksService.js
@@ -0,0 +1,11 @@
+import { appState } from "../AppState.js"
+
+
+class SnacksService {
+    buySnack(name) {
+        money = appState.money
+
+    }
+}
+
+export const snacksService = new SnacksService()
